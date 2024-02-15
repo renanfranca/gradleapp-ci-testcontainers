d7c0d06c50218adee915e1523b4e9d4c910769d5
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-02-15 12:59:32 -0300

    Apply gradle-java module

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+  java
+  jacoco
+  // jhipster-needle-gradle-plugins
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+jacoco {
+  toolVersion = libs.versions.jacoco.get()
+}
+
+// jhipster-needle-gradle-plugins-configurations
+
+repositories {
+  mavenCentral()
+  // jhipster-needle-gradle-repositories
+}
+
+group = "tech.jhipster.gradleapp"
+version = "0.0.1-SNAPSHOT"
+
+ext {
+  // jhipster-needle-gradle-properties
+}
+
+dependencies {
+  // jhipster-needle-gradle-dependencies
+  testImplementation(libs.junit.engine)
+  testImplementation(libs.junit.params)
+  testImplementation(libs.assertj)
+  testImplementation(libs.mockito)
+  // jhipster-needle-gradle-test-dependencies
+}
+
+tasks.test {
+  filter {
+    includeTestsMatching("*Test.*")
+    excludeTestsMatching("*IT.*")
+  }
+  useJUnitPlatform()
+  finalizedBy("jacocoTestReport")
+}
+
+val integrationTest = task<Test>("integrationTest") {
+  description = "Runs integration tests."
+  group = "verification"
+  shouldRunAfter("test")
+  filter {
+    includeTestsMatching("*IT.*")
+    excludeTestsMatching("*Test.*")
+  }
+  useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+  dependsOn("test", "integrationTest")
+  reports {
+    xml.required.set(true)
+    html.required.set(false)
+  }
+}
